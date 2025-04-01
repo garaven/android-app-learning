5d856bc2815baaf589942e9961873a5a4022f7a2
Camilo Avendaño <garaven.camilo@gmail.com>
2025-03-31 21:52:39 -0500

    feat: add extra attributes to  entity

--- app/src/main/java/com/example/calculatorandform/User.java
+++ app/src/main/java/com/example/calculatorandform/User.java
@@ -5,16 +5,22 @@ import androidx.annotation.NonNull;
 import java.io.Serializable;
 
 public class User implements Serializable {
-    String name, secondName, email;
+    String name, secondName, email, docType, gender;
     Integer age, id;
+    Long docNumber;
 
-    public User(String name, String secondName, String email, Integer age, Integer id) {
+    public User(String name, String secondName, String email, String docType, String gender, Integer age, Integer id, Long docNumber) {
         this.name = name;
         this.secondName = secondName;
         this.email = email;
         this.age = age;
         this.id = id;
+        this.docNumber = docNumber;
+        this.docType = docType;
+        this.gender = gender;
     }
+
+    // Set
 
     public void setName(String name) {
         this.name = name;
@@ -31,6 +37,20 @@ public class User implements Serializable {
     public void setAge(Integer age) {
         this.age = age;
     }
+
+    public void setDocNumber(Long docNumber) {
+        this.docNumber = docNumber;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public void setDocType(String docType) {
+        this.docType = docType;
+    }
+
+    // Get
 
     public String getName() {
         return name;
@@ -52,9 +72,15 @@ public class User implements Serializable {
         return id;
     }
 
-    @NonNull
-    @Override
-    public String toString() {
-        return "ID: " + id + ", Nombre: " + name + " " + secondName + ", Email: " + email + ", Edad: " + age;
+    public Long getDocNumber() {
+        return docNumber;
+    }
+
+    public String getDocType() {
+        return docType;
+    }
+
+    public String getGender() {
+        return gender;
     }
 }
